8cecd87ca02e736f3b2479cb256de47de41edbde
herrBez <bez.mirko@gmail.com>
2024-05-25 13:14:34 +0200

    Improve unit test and allow to test end-to-end Logstash conditions

--- internal/app/transpile/transpile_test.go
+++ internal/app/transpile/transpile_test.go
@@ -1,9 +1,12 @@
 package transpile
 
 import (
+	"fmt"
 	"testing"
 
+	config "github.com/breml/logstash-config"
 	ast "github.com/breml/logstash-config/ast"
+	"github.com/rs/zerolog/log"
 )
 
 func TestConversionOfConditions(t *testing.T) {
@@ -67,7 +70,7 @@ func TestConversionOfConditions(t *testing.T) {
 }
 
 // The function is called when converting selectors in if boolean expression
-func TestToElasticPipelineSelectorCondition(t *testing.T) {
+func TestToElasticPipelineSelectorConditionWithNullable(t *testing.T) {
 	tt := []struct {
 		name  string
 		input string
@@ -125,11 +128,66 @@ func TestToElasticPipelineSelector(t *testing.T) {
 			input: "[foo][bar]",
 			want:  `foo.bar`,
 		},
+		{
+			name:  "At case",
+			input: "[@metadata][test]",
+			want:  `@metadata.test`,
+		},
+		{
+			name:  "Without square brackets",
+			input: "justafield",
+			want:  `justafield`,
+		},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 			got := toElasticPipelineSelector(tc.input)
+			if tc.want != got {
+				t.Errorf("want \"%s\", got \"%s\"", tc.want, got)
+			}
+		})
+	}
+}
+
+// Help function to guarantee that a function returning a value and an error, returns only the value
+func dealWithError(i interface{}, err error) interface{} {
+	if err == nil {
+		return i
+	}
+	log.Panic().Msgf("Error: %v", err)
+	return err
+}
+
+// Instead of writing the parse tree manually, we do the following:
+// 1. Create a dummy filter
+// 2. Parse the dummy configuration
+// 3. Extract the condition
+func extractCondition(s string) ast.Condition {
+	return dealWithError(config.Parse("fake", []byte(fmt.Sprintf("filter { if %s {} }", s)))).(ast.Config).Filter[0].BranchOrPlugins[0].(ast.Branch).IfBlock.Condition
+}
+
+func TestEndToEnd(t *testing.T) {
+	tt := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "End-to-end test 1",
+			input: `[foo] == "foo"`,
+			want:  `(ctx?.foo != null && ctx.foo == "foo")`,
+		},
+		{
+			name:  "End-to-end test 2",
+			input: `[test]`,
+			want:  `ctx?.test != null`,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			got := transpileCondition(extractCondition(tc.input))
 			if tc.want != got {
 				t.Errorf("want \"%s\", got \"%s\"", tc.want, got)
 			}
